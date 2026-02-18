c28fa6e06206e5cd3543d2d8d2ee18c9a0c42218
Ansar <ansarsh1243@gmail.com>
2026-02-18 18:23:39 +0500

    auth: add login/register/me endpoints

--- backend/app/schemas/__init__.py
+++ backend/app/schemas/__init__.py
@@ -3,10 +3,11 @@ from app.schemas.department import *
 from app.schemas.employee import *
 from app.schemas.enums import *
 from app.schemas.supplier import *
-from app.schemas.token import Token
+from app.schemas.token import Token, TokenPayload
 from app.schemas.training import *
 from app.schemas.training_request import *
 
 __all__ = [
     "Token",
+    "TokenPayload",
 ]
